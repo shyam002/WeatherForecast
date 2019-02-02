8d041110d7544a49e5034be241b825b1a5f3c16e
Ghanshyam Gupta <ghanshyam.gupta@vxlsoftware.com>
2019-02-02 10:43:09 +0530

    Ask for Location permissions.

---
+++ app/src/main/java/com/example/administrator/weatherforecast/AskRunTimePermission.kt
@@ -0,0 +1,82 @@
+package com.example.administrator.weatherforecast
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+import java.util.ArrayList
+
+class AskRunTimePermission(internal var activity: Activity) {
+    internal lateinit var permissionarray: Array<String>
+
+    fun setPermissions(perlist: ArrayList<String>, requestcode: Int): Boolean {
+        println("call checkLocationPermissionAndEnableGps")
+        try {
+            if (Build.VERSION.SDK_INT >= 23) {
+                var shouldRequestPermissionWithMsg = false
+                var msg = activity.resources.getString(R.string.permssionmsg)
+                for (i in perlist.indices) {
+                    val permission = perlist[i]
+                    if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                            shouldRequestPermissionWithMsg = true
+                            if (!msg.isEmpty()) msg += "\n"
+                            msg += activity.resources.getString(R.string.bullet) + permission.replace(
+                                "android.permission.",
+                                " "
+                            )
+                        }
+                    }
+                }
+                if (!perlist.isEmpty()) {
+                    permissionarray = perlist.toTypedArray()
+                    if (shouldRequestPermissionWithMsg) {
+                        // Show an expanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+                        val alertDialogBuilder = AlertDialog.Builder(activity)
+                        alertDialogBuilder.setTitle("")
+                        alertDialogBuilder
+                            .setMessage(msg)
+                            .setCancelable(false)
+                            .setPositiveButton("OK") { dialog, _ ->
+                                dialog.cancel()
+                                ActivityCompat.requestPermissions(
+                                    activity,
+                                    permissionarray,
+                                    requestcode
+                                )
+                            }
+                            .setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+                        val alertDialog = alertDialogBuilder.create()
+                        alertDialog.show()
+                    } else {
+
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(
+                            activity,
+                            permissionarray,
+                            requestcode
+                        )
+
+                        // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                        // app-defined int  The callback method gets the
+                        // result of the request.
+                    }
+                } else {
+                    return true
+                }
+            } else {
+                return true
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return false
+    }
+}
--- app/src/main/java/com/example/administrator/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/example/administrator/weatherforecast/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.administrator.weatherforecast
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.Log
+import android.widget.Toast
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = MainActivity::class.java.simpleName
+    private val PermissionsRequestCode = 123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (setPermission()) {
+            Log.v(TAG,"Already have permission")
+        }
     }
+
+    fun setPermission(): Boolean {
+            val askRunTimePermission = AskRunTimePermission(this)
+            val perlist = ArrayList<String>()
+            perlist.add(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            perlist.add(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            return askRunTimePermission.setPermissions(perlist, PermissionsRequestCode)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        println("MainActivity onRequestPermissionsResult")
+        val granted = grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        when (requestCode) {
+            PermissionsRequestCode -> {
+                if (granted) {
+                    Log.v(TAG,"permission granted")
+                } else {
+                    setPermission()
+                }
+            }
+        }
+    }
+
+
 }
